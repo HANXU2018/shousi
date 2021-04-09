bf5f720764c71a03a616ceaaa1bb5086f9c0a35b
HANXU2018 <1076998404@qq.com>
2021-04-09 13:46:01 +0800

    Update HungrySingletonTest.java

--- shousicode/src/main/java/DesignPattern/singletion/HungrySingletonTest.java
+++ shousicode/src/main/java/DesignPattern/singletion/HungrySingletonTest.java
@@ -1,5 +1,13 @@
 package DesignPattern.singletion;
-
+/*
+* 饿汉式单例的优缺点
+优点：创建对象时没有加任何的锁、执行效率比较高。
+缺点：也很明显，
+* 因为其在类加载的时候就初始化了，
+* 也就是说不管我们用或者不用都占着空间，
+* 如果项目中有大量单例对象，
+* 则可能会浪费大量内存空间。
+* */
 public class HungrySingletonTest {
     public static void main(String[] args) {
         HungrySingleton singleton1 = HungrySingleton.getInstance();
